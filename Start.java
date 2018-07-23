b18a90633a81ab7e86f87be4e2c5d8aaf5a5afc5
Brandon Rosales <Rosales.Brandon.J@gmail.com>
2018-07-23 18:32:03 -0400

    Added starting java file

---
+++ Start.java
@@ -0,0 +1,15 @@
+public class Start{
+	public static void Main(){
+		Brandon brandon = new Brandon();
+		Christian christian = new Christian();
+		System.out.println("Brandon and Christian are having a conversation");
+		System.out.println("-----------------------------------------------");
+		speak(christian.name,christian.message);
+		speak(brandon.name,brandon.message);
+		System.out.println("Brandon and Christian nod with respect, and then part ways");
+	}
+
+	public static void speak(String name,String message){
+		System.out.printf("%s: $s",name,message);
+	}
+}
